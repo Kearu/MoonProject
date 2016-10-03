069e14cd4b2802a5f4eb751148f232163306bc0a
EdwardV <ejvenaglia@hotmail.com>
2016-10-03 13:54:58 -0600

    Added a thread to call moon animation class & progressed AnimateMoon class

--- src/AnimatedMoon.java
+++ src/AnimatedMoon.java
@@ -1,9 +1,26 @@
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
+import java.lang.Math;
 
 public class AnimatedMoon extends GraphicsProgram  {
 	
+	private double x, y;
+	private double length = 50;
+	private double angle = 0.0;
+	private double angle_stepsize = 0.1;
+	
+	//This needs to be worked on. Moon needs to circle the Earths Center Point.
 	public void run () {
+		
+		while (angle < 2 * Math.PI)
+	    {
+	        // calculate x, y from a vector with known length and angle
+	        x = length * Math.cos(angle);
+	        y = length * Math.sin(angle);
+	        
+	        
+	        angle += angle_stepsize;
+	    }
 		
 	}
 
--- src/EarthAndMoon.java
+++ src/EarthAndMoon.java
@@ -2,24 +2,26 @@
 // this is ANOTHER comment
 
 // omg hi
-
+import java.lang.Thread;
 import acm.program.*;
 import acm.graphics.*;
 import java.awt.*;
 
 public class EarthAndMoon extends GraphicsProgram {
 	
-	public static final int MOON_RADIUS = 3 * 5;
-	public static final int EARTH_RADIUS = 13 * 5;
+	public static final int MOON_RADIUS = 3 * 10;
+	public static final int EARTH_RADIUS = 13 * 10;
 	
 	public void run() {
+		this.resize(1500,1500);
 		setSize(600, 400);
+		//this.resize(1500,1500);
 		
-		int moonWidth = (getWidth() - 75) / 2;
-		int moonHeight = (getHeight() - 75) / 2;
+		int moonWidth = (getWidth() + 1700) / 2;
+		int moonHeight = (getHeight() + 1250) / 2;
 		
-		int earthWidth = (getWidth()) / 2;
-		int earthHeight = (getHeight()) / 2;
+		int earthWidth = (getWidth() + 900) / 2;
+		int earthHeight = (getHeight()+ 700) / 2;
 		
 		moon(moonWidth, moonHeight, MOON_RADIUS, true, Color.BLACK);
 		earth(earthWidth, earthHeight, EARTH_RADIUS, true, Color.RED);
@@ -33,10 +35,19 @@ public class EarthAndMoon extends GraphicsProgram {
 	}
 	
 	public void moon(int x, int y, int radius, boolean isFilled, Color color) {
+		
 		GOval circle = new GOval(x, y, radius, radius);
 		circle.setFilled(true);
 		circle.setColor(color);
 		add(circle);
+		
+		//animate moon called as a thread, so it will run in the background.
+		
+		AnimatedMoon startAnimation = new AnimatedMoon();
+		Thread moonAni = new Thread(startAnimation);
+		moonAni.start();
+		
+		
 	}
 	
 	public void moonOrbit () {
