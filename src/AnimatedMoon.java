dc88e0ed2efe5307a7b7f17817101ef291f7de69
EdwardVenaglia <Ejvenaglia@hotmail.com>
2016-10-03 16:03:35 -0600

    Added more unfuctioning code, getting closer to orbit!

--- src/AnimatedMoon.java
+++ src/AnimatedMoon.java
@@ -1,11 +1,13 @@
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
+
+import java.awt.Color;
 import java.lang.Math;
 
 public class AnimatedMoon extends GraphicsProgram  {
 	
 	private double x, y;
-	private double length = 50;
+	private double length = 400;
 	private double angle = 0.0;
 	private double angle_stepsize = 0.1;
 	
@@ -14,14 +16,29 @@ public class AnimatedMoon extends GraphicsProgram  {
 		
 		while (angle < 2 * Math.PI)
 	    {
+			
 	        // calculate x, y from a vector with known length and angle
 	        x = length * Math.cos(angle);
 	        y = length * Math.sin(angle);
-	        
+	        EarthAndMoon.moonWidth = x;
+	        EarthAndMoon.moonHeight = y;
 	        
 	        angle += angle_stepsize;
+	        moon(EarthAndMoon.moonWidth, EarthAndMoon.moonHeight, EarthAndMoon.MOON_RADIUS, true, Color.GRAY);
 	    }
 		
+	}
+	public void moon(double x, double y, int radius, boolean isFilled, Color color) {
+		
+		GOval circle = new GOval(x, y, radius, radius);
+		circle.setFilled(true);
+		circle.setColor(color);
+		pause(300);
+		
+		add(circle);
+		
+		
+		//animate moon called as a thread, so it will run in the background.
 	}
 
 }
--- src/EarthAndMoon.java
+++ src/EarthAndMoon.java
@@ -8,20 +8,26 @@ public class EarthAndMoon extends GraphicsProgram {
 	
 	public static final int MOON_RADIUS = 3 * 10;
 	public static final int EARTH_RADIUS = 13 * 10;
+	public static double moonWidth;
+	public static double moonHeight;
 	
 	public void run() {
-		this.resize(1500,1500);
-		setSize(600, 400);
+		this.resize(2500,2500);
+		//setSize(600, 400);
 		//this.resize(1500,1500);
 		
-		int moonWidth = (getWidth() + 1700) / 2;
-		int moonHeight = (getHeight() + 1250) / 2;
+		moonWidth = (getWidth() + 1700) / 2;
+		moonHeight = (getHeight() + 1250) / 2;
 		
 		int earthWidth = (getWidth() + 900) / 2;
 		int earthHeight = (getHeight()+ 700) / 2;
 		
-		moon(moonWidth, moonHeight, MOON_RADIUS, true, Color.BLACK);
+		//moon(moonWidth, moonHeight, MOON_RADIUS, true, Color.BLACK);
 		earth(earthWidth, earthHeight, EARTH_RADIUS, true, Color.RED);
+		AnimatedMoon startAnimation = new AnimatedMoon();
+		Thread moonAni = new Thread(startAnimation);
+		moonAni.start();
+		startAnimation.run();
 	}
 	
 	public void earth(int x, int y, int radius, boolean isFilled, Color color) {
@@ -31,7 +37,7 @@ public class EarthAndMoon extends GraphicsProgram {
 		add(circle);
 	}
 	
-	public void moon(int x, int y, int radius, boolean isFilled, Color color) {
+	public void moon(double x, double y, int radius, boolean isFilled, Color color) {
 		
 		GOval circle = new GOval(x, y, radius, radius);
 		circle.setFilled(true);
