c785c99cdd1c86c26c186a6b0e871dcea4c1d2e6
Kyle Melius <Kyle Melius>
2016-10-03 10:30:26 -0600

    Project uploaded to git

---
+++ src/EarthAndMoon.java
@@ -0,0 +1,45 @@
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.*;
+
+public class EarthAndMoon extends GraphicsProgram {
+	
+	public static final int MOON_RADIUS = 3 * 5;
+	public static final int EARTH_RADIUS = 13 * 5;
+	
+	public void run() {
+		setSize(600, 400);
+		
+		int moonWidth = (getWidth() - 75) / 2;
+		int moonHeight = (getHeight() - 75) / 2;
+		
+		int earthWidth = (getWidth()) / 2;
+		int earthHeight = (getHeight()) / 2;
+		
+		moon(moonWidth, moonHeight, MOON_RADIUS, true, Color.BLACK);
+		earth(earthWidth, earthHeight, EARTH_RADIUS, true, Color.RED);
+	}
+	
+	public void earth(int x, int y, int radius, boolean isFilled, Color color) {
+		GOval circle = new GOval(x, y, radius, radius);
+		circle.setFilled(true);
+		circle.setColor(color);
+		add(circle);
+	}
+	
+	public void moon(int x, int y, int radius, boolean isFilled, Color color) {
+		GOval circle = new GOval(x, y, radius, radius);
+		circle.setFilled(true);
+		circle.setColor(color);
+		add(circle);
+	}
+	
+	public void moonOrbit () {
+		
+	}
+	
+	public void missile() {
+		
+	}
+
+}
